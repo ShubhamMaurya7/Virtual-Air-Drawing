291d75a14e7c5242b13c41a1e42b5e25bc0abb28
Shubham Maurya <151072431+ShubhamMaurya7@users.noreply.github.com>
2025-05-25 19:11:15 +0530

    Add files via upload

---
+++ hand_detector.py
@@ -0,0 +1,39 @@
+# utils/hand_detector.py
+
+import cv2
+import mediapipe as mp
+
+class HandDetector:
+    def __init__(self, mode=False, max_hands=1, detection_confidence=0.8, tracking_confidence=0.8):
+        self.mode = mode
+        self.max_hands = max_hands
+        self.detection_confidence = detection_confidence
+        self.tracking_confidence = tracking_confidence
+
+        self.mpHands = mp.solutions.hands
+        self.hands = self.mpHands.Hands(
+            static_image_mode=self.mode,
+            max_num_hands=self.max_hands,
+            min_detection_confidence=self.detection_confidence,
+            min_tracking_confidence=self.tracking_confidence
+        )
+        self.mpDraw = mp.solutions.drawing_utils
+
+    def find_hands(self, img, draw=True):
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        self.results = self.hands.process(imgRGB)
+        if self.results.multi_hand_landmarks:
+            for handLms in self.results.multi_hand_landmarks:
+                if draw:
+                    self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+        return img
+
+    def find_position(self, img, hand_no=0):
+        lm_list = []
+        if self.results.multi_hand_landmarks:
+            my_hand = self.results.multi_hand_landmarks[hand_no]
+            for id, lm in enumerate(my_hand.landmark):
+                h, w, _ = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                lm_list.append((id, cx, cy))
+        return lm_list
